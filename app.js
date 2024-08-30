3693d3f497afab7b201bba909b630d5877b46362
mujtabashamas <mujtabashamasdev@gmail.com>
2024-08-30 14:47:44 +0500

    init

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ io.on("connection", (socket) => {
 })
 
 
-app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/frontend/dist/index.html"));
