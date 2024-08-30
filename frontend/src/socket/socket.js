54fc6811467f0e6f0d5659a8c830998700d6ef37
mujtabashamas <mujtabashamasdev@gmail.com>
2024-08-30 15:04:05 +0500

    build

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,9 +1,6 @@
 import { io } from 'socket.io-client';
 
 // Connect to the same URL where both frontend and backend are hosted
-const socket = io('https://coco-chat-c426881ffc0e.herokuapp.com', {
-  transports: ['websocket'], // Ensures WebSocket transport is used
-  withCredentials: true, // Ensure credentials are sent for authentication, if needed
-});
+const socket = io('https://coco-chat-c426881ffc0e.herokuapp.com');
 
 export default socket;
