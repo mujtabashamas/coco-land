56952c0dc2a8e0ee333ed432840151207ac592ed
mujtabashamas <mujtabashamasdev@gmail.com>
2024-09-15 14:37:51 +0500

    changes

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,13 +1,21 @@
 import { io } from 'socket.io-client';
-import { getUser } from '../features/userSlice'
+// import { getUser } from '../features/userSlice'
 
-const userData = getUser();
+// const userData = getUser();
 // Connect to the same URL where both frontend and backend are hosted
-// const socket = io('http://localhost:3001');
-const socket = io('/', {
-  query: {
-    user: userData
-  }
-});
+
+// const socket = io('http://localhost:3001',
+//   {
+//     query: {
+//       user: userData
+//     }
+//   });
+
+// const socket = io('/', {
+//   query: {
+//     user: userData
+//   }
+// });
+const socket = io('/');
 
 export default socket;
